21b45d3205c1ef00c1d7d17fd033cb2fffba96c5
HamJiwon <skxo0039@gmail.com>
2026-08-02 14:39:41 +0900

    add: generic 타입 위치 변경

--- src/main/java/hello/ticketing/service/ReservationService.java
+++ src/main/java/hello/ticketing/service/ReservationService.java
@@ -1,6 +1,10 @@
 package hello.ticketing.service;
 
+import hello.ticketing.domain.Reservation;
+import hello.ticketing.dto.request.CreateReservationRequest;
+import hello.ticketing.dto.response.ReservationResponse;
+
 public interface ReservationService {
 
-
+    ReservationResponse create(Long userId, Long roundId, int quantity);
 }
