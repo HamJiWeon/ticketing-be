9766206ba1f8b7a8963c27de7e23a7ef67a9e1fa
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-03 06:10:59 +0900

    feat: performance 엔티티 수정 - createdAt, updatedAt 필드 추가 - builder 추가

--- src/main/java/hello/ticketing/domain/Performance.java
+++ src/main/java/hello/ticketing/domain/Performance.java
@@ -2,14 +2,20 @@ package hello.ticketing.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Performance {
 
@@ -35,4 +41,25 @@ public class Performance {
 
     @OneToMany(mappedBy = "performance")
     private List<Round> round;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Performance(Long id, String name, String place, int price, LocalDate startAt, LocalDate endAt, GenreStatus genre, LimitStatus ticketLimit, List<Round> round, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.place = place;
+        this.price = price;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.genre = genre;
+        this.ticketLimit = ticketLimit;
+        this.round = round;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
