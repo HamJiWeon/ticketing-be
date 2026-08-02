c51c29a7393a8cca43310416152e2823dfb81040
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-03 06:20:22 +0900

    feat: repository, service, mapper 구현 - 중복되는 공연이 있는지 찾기 위해 이름+장소+장르가 존재하는지 쿼리 메서드 작성 - create 서비스 로직 구현 - mapstruct 의존성 추가 - dto로 변환해주는 mapper 구현

--- src/main/java/hello/ticketing/service/PerformanceService.java
+++ src/main/java/hello/ticketing/service/PerformanceService.java
@@ -1,6 +1,9 @@
 package hello.ticketing.service;
 
+import hello.ticketing.dto.request.PerformanceCreateRequest;
+import hello.ticketing.dto.response.PerformanceResponse;
+
 public interface PerformanceService {
 
-
+    PerformanceResponse create(PerformanceCreateRequest request);
 }
--- src/main/java/hello/ticketing/service/PerformanceServiceImpl.java
+++ src/main/java/hello/ticketing/service/PerformanceServiceImpl.java
@@ -1,5 +1,10 @@
 package hello.ticketing.service;
 
+import hello.ticketing.domain.Performance;
+import hello.ticketing.dto.request.PerformanceCreateRequest;
+import hello.ticketing.dto.response.PerformanceResponse;
+import hello.ticketing.mapper.PerformanceMapper;
+import hello.ticketing.repository.PerformanceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,5 +12,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class PerformanceServiceImpl {
+public class PerformanceServiceImpl implements PerformanceService {
+
+    private final PerformanceRepository performanceRepository;
+    private final PerformanceMapper performanceMapper;
+
+    @Override
+    public PerformanceResponse create(PerformanceCreateRequest request) {
+        if(performanceRepository.existsByNameAndPlaceAndGenre(request.name(), request.place(), request.genre())) {
+            throw new IllegalArgumentException("이미 존재하는 공연입니다.");
+        }
+
+        Performance performance = Performance.builder()
+                .name(request.name())
+                .place(request.place())
+                .price(request.price())
+                .startAt(request.startAt())
+                .endAt(request.endAt())
+                .genre(request.genre())
+                .ticketLimit(request.ticketLimit())
+                .build();
+
+        Performance save = performanceRepository.save(performance);
+        return performanceMapper.toDto(save);
+    }
 }
