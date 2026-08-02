429e2aab9d428e0bfc80f6045e3fb91c5ee2266f
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-03 06:14:42 +0900

    refactor: dto 추가 - PerformanceCreateRequest에 검증 어노테이션 추가, genre 필드 추가 - PerformanceDto에서 PerformanceResponse로 이름 변경, genre 필드 추

--- src/main/java/hello/ticketing/dto/response/PerformanceResponse.java
+++ src/main/java/hello/ticketing/dto/response/PerformanceResponse.java
@@ -1,18 +1,20 @@
 package hello.ticketing.dto.response;
 
+import hello.ticketing.domain.GenreStatus;
 import hello.ticketing.domain.LimitStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record PerformanceDto(
+public record PerformanceResponse(
         Long id,
         String name,
         LocalDate startAt,
         LocalDate endAt,
         String place,
         int price,
-        LimitStatus status,
+        GenreStatus genre,
+        LimitStatus ticketLimit,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
