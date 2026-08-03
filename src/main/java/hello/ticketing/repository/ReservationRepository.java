f93a2ae65f6ccfcb2513e89007afefaed50f0da0
HamJiwon <skxo0039@gmail.com>
2026-08-03 17:36:55 +0900

    feat: 유저별 예약 목록 조회를 위한 findByUser_Id 쿼리 메서드 추가

--- src/main/java/hello/ticketing/repository/ReservationRepository.java
+++ src/main/java/hello/ticketing/repository/ReservationRepository.java
@@ -1,7 +1,12 @@
 package hello.ticketing.repository;
 
 import hello.ticketing.domain.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+import java.util.UUID;
+
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    Page<Reservation> findByUser_Id(Long userId, Pageable pageable);
 }
