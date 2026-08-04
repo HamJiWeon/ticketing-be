42319ad60a2616fb8bcfd3ab08cd71b43362eeb0
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-05 00:22:48 +0900

    fix: 레포지토리 수정 - 이름+장소+장르로 중복 저장되는 로직을 제거했기 때문에 같이 제거

--- src/main/java/hello/ticketing/repository/PerformanceRepository.java
+++ src/main/java/hello/ticketing/repository/PerformanceRepository.java
@@ -1,10 +1,7 @@
 package hello.ticketing.repository;
 
-import hello.ticketing.domain.GenreStatus;
 import hello.ticketing.domain.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PerformanceRepository extends JpaRepository<Performance, Long> {
-
-    boolean existsByNameAndPlaceAndGenre(String name, String place, GenreStatus genre);
 }
