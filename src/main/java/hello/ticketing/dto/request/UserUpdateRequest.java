904e34b4bb47a6fc26b2ca6d3766ada7ef50e3b2
K-Dayeon03 <zns030@gmail.com>
2026-08-05 15:10:44 +0900

    feat: User dto 수정

--- src/main/java/hello/ticketing/dto/request/UserUpdateRequest.java
+++ src/main/java/hello/ticketing/dto/request/UserUpdateRequest.java
@@ -4,14 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
-        @NotBlank
-        @Size(min = 8)
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
         String password,
 
-        @NotBlank
+        @NotBlank(message = "전화번호는 필수입니다.")
         String phone,
 
-        @NotBlank
+        @NotBlank(message = "주소는 필수입니다.")
         String address
 ) {
 }
