d751597a8d2c6b5d3177c5d23f9521f64286053d
강다연 <142641618+K-Dayeon03@users.noreply.github.com>
2026-08-05 15:14:01 +0900

    Feat: User DTO 수정 (#12)
    * feat: CodeRabbit AI 코드 리뷰 기능 추가
    
    * refactor: application.properties 파일 변경
    - username, password 값 변경
    
    * feat: PULL_REQUEST_TEMPLATE.md 추가
    
    * feat: User API 구현
    
    * feat: User API 구현
    
    * feat: User dto 수정
    
    ---------
    
    Co-authored-by: HamJiwon <skxo0039@gmail.com>

--- src/main/java/hello/ticketing/dto/request/UserCreateRequest.java
+++ src/main/java/hello/ticketing/dto/request/UserCreateRequest.java
@@ -1,27 +1,31 @@
 package hello.ticketing.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
-        @NotBlank
+
+        @NotBlank(message = "이름은 필수입니다.")
         String name,
 
-        @NotBlank
-        @Size(min = 8)
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
         String password,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이어야 합니다.")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "전화번호는 필수입니다.")
         String phone,
 
+        @Min(value = 8, message = "나이는 8세 이상이어야 합니다.")
         int age,
 
-        @NotBlank
+        @NotBlank(message = "주소는 필수입니다.")
         String address
 ) {
 }
